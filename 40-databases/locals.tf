c520c526b4a75d12648e711e99bd884adf17be67
Dinesh velpula <velpuladinesh887@gmail.com>
2026-01-04 18:01:56 +0530

    terraform

--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -1,7 +1,8 @@
 locals {
+  common_name_suffix = "${var.project_name}-${var.environment}"
   ami_id = data.aws_ami.joindevops.id
-  sg_id = data.aws_ssm_parameter.bastion_sg_id.value
-  public_subnet_id = split("," , data.aws_ssm_parameter.public_subnet_ids.value)[0]
+  mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
+  database_subnet_id = split("," , data.aws_ssm_parameter.database_subnet_ids.value)[0]
   common_tags = {
     project_name = var.project_name
     environment = var.environment
